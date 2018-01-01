9574dfc6b13b5ca0483b3454545af66b63cf95a7
Stewart ilondanga <stilondanga@gmail.com>
2018-01-01 14:58:44 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
 //fetching HTML Elements in variables by ID
 var x = document.getElementById("form_sample");
 
+var createform = document.createElement('form'); // Create New Element form
+ createform.setAttribute("action", ""); // Setting action Attribute on form
+ createform.setAttribute("method", "post"); // Setting method Attribute on form
+ x.appendChild(createform);
 
+ 
 
 var navigate = (function() {
 	$('.dd').toggle();
