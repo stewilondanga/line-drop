1a221ced5d6b601ce53f8039ef70585d34a21d4b
Stewart ilondanga <stilondanga@gmail.com>
2018-01-01 15:03:57 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -2,68 +2,68 @@
 var x = document.getElementById("form_sample");
 
 var createform = document.createElement('form'); // Create New Element form
- createform.setAttribute("action", ""); // Setting action Attribute on form
- createform.setAttribute("method", "post"); // Setting method Attribute on form
- x.appendChild(createform);
+createform.setAttribute("action", ""); // Setting action Attribute on form
+createform.setAttribute("method", "post"); // Setting method Attribute on form
+x.appendChild(createform);
 
- var heading = document.createElement('h2'); // Heading of form
- heading.innerHTML = "Contact Form ";
- createform.appendChild(heading);
+var heading = document.createElement('h2'); // Heading of form
+heading.innerHTML = "Contact Form ";
+createform.appendChild(heading);
 
- var line = document.createElement('hr'); //giving horizontal row after heading
- createform.appendChild(line);
+var line = document.createElement('hr'); //giving horizontal row after heading
+createform.appendChild(line);
 
- var linebreak = document.createElement('br');
- createform.appendChild(linebreak);
+var linebreak = document.createElement('br');
+createform.appendChild(linebreak);
 
- var namelabel = document.createElement('label'); // Create Label for name field
- namelabel.innerHTML = "Your Name : "; // Set Field Labels
- createform.appendChild(namelabel);
+var namelabel = document.createElement('label'); // Create Label for name field
+namelabel.innerHTML = "Your Name : "; // Set Field Labels
+createform.appendChild(namelabel);
 
- var inputelement = document.createElement('input'); // Create input field for name
- inputelement.setAttribute("type", "text");
- inputelement.setAttribute("name", "dname");
- createform.appendChild(inputelement);
+var inputelement = document.createElement('input'); // Create input field for name
+inputelement.setAttribute("type", "text");
+inputelement.setAttribute("name", "dname");
+createform.appendChild(inputelement);
 
- var linebreak = document.createElement('br');
- createform.appendChild(linebreak);
+var linebreak = document.createElement('br');
+createform.appendChild(linebreak);
 
- var emaillabel = document.createElement('label'); //Create Label for email field
- emaillabel.innerHTML = "Your Email : ";
- createform.appendChild(emaillabel);
+var emaillabel = document.createElement('label'); //Create Label for email field
+emaillabel.innerHTML = "Your Email : ";
+createform.appendChild(emaillabel);
 
- var emailelement = document.createElement('input'); // Create input field for email
- emailelement.setAttribute("type", "text");
- emailelement.setAttribute("name", "demail");
- createform.appendChild(emailelement);
+var emailelement = document.createElement('input'); // Create input field for email
+emailelement.setAttribute("type", "text");
+emailelement.setAttribute("name", "demail");
+createform.appendChild(emailelement);
 
 var emailbreak = document.createElement('br');
- createform.appendChild(emailbreak);
+createform.appendChild(emailbreak);
 
- // Append Textarea
+// Append Textarea
 var messagelabel = document.createElement('label');
- messagelabel.innerHTML = "Your Message : ";
- createform.appendChild(messagelabel);
+messagelabel.innerHTML = "Your Message : ";
+createform.appendChild(messagelabel);
 
- var texareaelement = document.createElement('textarea');
- texareaelement.setAttribute("name", "dmessage");
- createform.appendChild(texareaelement);
+var texareaelement = document.createElement('textarea');
+texareaelement.setAttribute("name", "dmessage");
+createform.appendChild(texareaelement);
 
 var messagebreak = document.createElement('br');
- createform.appendChild(messagebreak);
+createform.appendChild(messagebreak);
 
 // Append Submit Button
 var submitelement = document.createElement('input');
- submitelement.setAttribute("type", "submit");
- submitelement.setAttribute("name", "dsubmit");
- submitelement.setAttribute("value", "Submit");
- createform.appendChild(submitelement);
+submitelement.setAttribute("type", "submit");
+submitelement.setAttribute("name", "dsubmit");
+submitelement.setAttribute("value", "Submit");
+createform.appendChild(submitelement);
 
 var navigate = (function() {
-	$('.dd').toggle();
-	$('.dd_btn').click(function() {
-		var dataName = $(this).attr('data-name');
-		$('.dd').hide();
-		$('.' + dataName).toggle();
-	});
+  $('.dd').toggle();
+  $('.dd_btn').click(function() {
+    var dataName = $(this).attr('data-name');
+    $('.dd').hide();
+    $('.' + dataName).toggle();
+  });
 })();
